a6fe2cf14ddab32d00fe9bb304ad202bd29e5c83
deepak7740 <84527151+deepak7740@users.noreply.github.com>
2022-03-13 18:47:31 +0530

    Create pattern4.java

---
+++ PATTERN/pattern4.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class pattern4
+{
+	public static void main(String args[])
+	 {
+        for (int i = 0; i < 5; i++)
+         {
+            for (int j = 0; j < 5 - i; j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++)
+             {
+                System.out.print( "* ");
+            }
+           
+ System.out.println();
+ }
+	}
+}
